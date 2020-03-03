8a315e4df6a6b034dda314cc77a2a12c8ec9caa9
sakurai gaku <sakurai_gaku_333@altpaper.net>
2020-03-03 11:39:28 +0900

    a

--- src/main/scala/Chapter.scala
+++ src/main/scala/Chapter.scala
@@ -1,6 +1,7 @@
 object Run extends App {
 //  println(Chapter2.fib(4))
   println(Chapter2.isSorted(Seq(4, 2, 3).toArray, testIsSorted))
+
   def testIsSorted(x: Int, y: Int): Boolean = x < y
 }
 
@@ -16,4 +17,33 @@ object Chapter2 {
 
     go(0)
   }
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    a => b => f(a, b)
+  }
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a, b) => f(a)(b)
+  }
+
+  def conpose[A, B, C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
+  }
+}
+
+object Chaptar3 {
+
+  sealed trait List[+A]
+
+  case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+  case object Nil extends List[Nothing]
+
+  object List {
+    def sum(ints: List[Int]): Int = ints match {
+      case Nil => 0
+      case Cons(x, xs) => x + sum(xs)
+    }
+  }
+
 }
